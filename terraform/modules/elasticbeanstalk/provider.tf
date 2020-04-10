3bf8327f28d031e294c42c281e7cf6cc46443ad4
jagdevops19 <56228305+jagdevops19@users.noreply.github.com>
2020-04-10 19:48:21 +0100

    Update provider.tf

--- terraform/modules/elasticbeanstalk/provider.tf
+++ terraform/modules/elasticbeanstalk/provider.tf
@@ -1,5 +1,3 @@
 provider "aws" { 
-    region = "${var.AWS_REGION}"
-    access_key = "AKIATQADRHDA6JCCULWQ"
-    secret_key = "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"
+    region = "${var.AWS_REGION}"  
 }
